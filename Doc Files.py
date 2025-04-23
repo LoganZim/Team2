f6386f2df3439d499dbe4e306a24f8d6a8e6e5b9
Ryan-Pool <rp125328@student.wpsok.org>
2025-04-23 13:46:20 -0500

    Doc files for sprints, and accounts

---
+++ Doc Files.py
@@ -0,0 +1,5 @@
+#Google doc to story outline. Add what you want
+#https://docs.google.com/document/d/1GZH_bh_P-sCS0AJxllb1aa1ArQJpDFE0UFREKYrY7A4/edit?usp=sharing
+
+#Google doc for sprints and daily accounts for when I am gone
+#https://docs.google.com/document/d/1Nf8NS5SYq5r-W5JxybsTVA2rOSIt7oYDnqMa_4sEEkE/edit?usp=sharing
